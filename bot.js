161aeb18acf94847c32207229a3a80a00cbefd91
Kevin Nguyen <KevinNguyenn@utexas.edu>
2017-01-26 15:10:36 -0600

    Update duplicate message code

--- bot.js
+++ bot.js
@@ -94,45 +94,15 @@ function removeUser(trollUser) {
         }
       });
       botReq.end();
+      postMessage("Eliminated. Life will be better without their EXTRA presence. ");
     }
-    
-
-    var botResponse = "eliminated. Life will be better without their EXTRA presence. " + cool();
     
     var warningUserIndex = warningUsers.indexOf(trollUser);
     warningUsers.splice(warningUserIndex, 1);
     
     var trollUserIndex = trollUsers.indexOf(trollUser);
     trollUsers.splice(trollUserIndex, 1);
-    
-    options = {
-        hostname: 'api.groupme.com',
-        path: '/v3/bots/post',
-        method: 'POST'
-    };
 
-      body = {
-        "bot_id" : botID,
-        "text" : botResponse
-      };
-
-      console.log('sending ' + botResponse + ' to ' + botID);
-
-      botReq = HTTPS.request(options, function(res) {
-          if(res.statusCode == 202) {
-            //neat
-          } else {
-            console.log('rejecting bad status code ' + res.statusCode);
-          }
-      });
-
-      botReq.on('error', function(err) {
-        console.log('error posting message '  + JSON.stringify(err));
-      });
-      botReq.on('timeout', function(err) {
-        console.log('timeout posting message '  + JSON.stringify(err));
-      });
-      botReq.end(JSON.stringify(body));
 }
 
 function getGroupInfo() {
